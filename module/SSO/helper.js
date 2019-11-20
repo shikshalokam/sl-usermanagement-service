8a0b4677380caafa6655a9b0e8d1fba234a4cc12
aman-tunerlabs <aman@tunerlabs.com>
2019-11-20 12:58:33 +0530

    added SSO functionality

---
+++ module/SSO/helper.js
@@ -0,0 +1,20 @@
+module.exports = class notificationsHelper {
+
+    static login(ssoData) {
+        return new Promise(async (resolve, reject) => {
+            try {
+
+                console.log("here")
+
+                return resolve({
+                    status: 200,
+                    result: "Success"
+                })
+            }
+            catch (error) {
+                return reject(error);
+            }
+        })
+    }
+
+};
